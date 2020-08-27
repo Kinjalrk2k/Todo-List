3ec0a3e8953b31e08aa430bd78270857c85ce4ba
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-08-28 00:13:06 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// check off todos by clicking
+$("li").click(function () {
+  $(this).toggleClass("completed");
+});
+
+// delete todo
+$("span").click(function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
