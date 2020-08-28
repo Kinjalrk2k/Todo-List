0f124c07c4f8fd1ed39251933d7c432cb1321502
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-08-28 10:23:52 +0530

    Adding todo functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 // check off todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
 // delete todo
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
   $(this)
     .parent()
     .fadeOut(500, function () {
       $(this).remove();
     });
   event.stopPropagation();
+});
+
+// create todo
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+    $(this).val("");
+  }
 });
